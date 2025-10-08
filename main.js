2eac2300a50a8d29e14c3d1677bee65a19027e63
Vincent Dao <vincentdao288@gmail.com>
2025-10-08 15:08:14 +1100

    Mobile menu complete.

---
+++ main.js
@@ -0,0 +1,59 @@
+let hamburgerIcon = document.querySelector(".header__hamburgerIcon");
+let closingIcon = document.querySelector(".header__closingIcon");
+
+let textPath = document.querySelector(".logoIcon__textPath");
+let bookmarkInner = document.querySelector(".logoIcon__bookmarkInner");
+let logoCircle = document.querySelector("circle");
+
+let nav = document.querySelector("nav");
+
+function setNavActive(){
+  // Sets closing as active, hides hamburger
+  hamburgerIcon.style.display = "none";
+  hamburgerIcon.setAttribute("inert", true);
+  closingIcon.style.display = "block";
+  closingIcon.removeAttribute("inert");
+  closingIcon.focus();
+
+  // Adds active states to logo to allow for color change
+  textPath.classList.add("active");
+  bookmarkInner.classList.add("active");
+  logoCircle.classList.add("active");
+  
+  // Finally, set nav component as active
+  nav.removeAttribute("inert");
+  nav.classList.add("active");
+}
+hamburgerIcon.addEventListener("click", () => {
+  setNavActive();
+});
+hamburgerIcon.addEventListener("keydown", (e) => {
+  if(e.key === "Enter" || e.key === " "){
+    e.preventDefault();
+    setNavActive();
+  }
+});
+
+function setNavInactive(){
+  closingIcon.style.display = "none";
+  closingIcon.setAttribute("inert", true);
+  hamburgerIcon.style.display = "block";
+  hamburgerIcon.removeAttribute("inert");
+  hamburgerIcon.focus();
+
+  textPath.classList.remove("active");
+  bookmarkInner.classList.remove("active");
+  logoCircle.classList.remove("active");
+
+  nav.setAttribute("inert", true);
+  nav.classList.remove("active");
+}
+closingIcon.addEventListener("click", () => {
+  setNavInactive();
+});
+closingIcon.addEventListener("keydown", (e) => {
+  if(e.key === "Enter" || e.key === " "){
+    e.preventDefault();
+    setNavInactive();
+  }
+});
