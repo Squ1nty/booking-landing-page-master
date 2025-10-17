e7108754afc551edf190ce68c7c0a62246375363
Vincent Dao <vincentdao288@gmail.com>
2025-10-17 13:11:36 +1100

    Updated config files to include JS

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? 'bookmark-landing-page-master' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
